2f6844b163d22712b85cb9a45af24141bb93c4a7
Vaibhav Shrivastava <iammagnificient@gmail.com>
2021-02-19 17:28:19 +0530

    [Vaibhav] Machine Req Fulfilled

--- ticket-tracker-backend/src/machines/routers.py
+++ ticket-tracker-backend/src/machines/routers.py
@@ -3,8 +3,9 @@ from .viewsets import ManufacturerViewSet, MachineViewSet
 
 
 api_router = SimpleRouter()
-api_router.register("manufacturer", ManufacturerViewSet, basename="manufacturer-api")
 api_router.register("machine", MachineViewSet, basename="machine-api")
+api_router.register("manufacturer", ManufacturerViewSet, basename="manufacturer-api")
+
 
 api_routes = api_router.urls
 
