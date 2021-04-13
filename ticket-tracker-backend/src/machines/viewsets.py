31da2a9bfc06f29f47634b331cf044606c24f341
Vaibhav Shrivastava <iammagnificient@gmail.com>
2021-04-13 14:32:36 +0530

    Enabled corsheaders

--- ticket-tracker-backend/src/machines/viewsets.py
+++ ticket-tracker-backend/src/machines/viewsets.py
@@ -18,9 +18,9 @@ class MachineViewSet(ModelViewSet):
 
     queryset = Machine.objects.all()
     serializer_class = MachineSerializer
-    lookup_field = "slug"
+    lookup_field = "__all__"
 
-    @action(detail=True, methods=["HEAD", "GET", "POST"])
+    @action(detail=True, methods=["HEAD, GET, POST"])
     def manufacturers(self, request, slug=None):
         machine = self.get_object()
         if request.method in ("HEAD", "GET"):
